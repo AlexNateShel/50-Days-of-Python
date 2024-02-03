4ac7b973c68e0bde0c641dd0ccb2eda3852264a1
Alexander Shelton <alexander.nathan.shelton@outlook.com>
2024-02-03 00:05:54 -0500

    Day 2 - Started

---
+++ Day 2/check_duplicates.py
@@ -0,0 +1,7 @@
+# Write a function called 'check_duplicates' that takes a list of strings as an argument.
+# The function should check if the list has any duplicates.
+# If there are duplicates, the function should return a list of duplicates.
+# If there are no duplicates, the function should return "no duplicates". 
+# For example, the list of fruits below should return ["apple", "banana"], and the list of names should return "no duplicates".
+# fruits = ['apple', 'orange', 'banana', 'apple', 'banana']
+# names = ['Yoda', 'Moses', 'Joshua', 'Mark'] 
