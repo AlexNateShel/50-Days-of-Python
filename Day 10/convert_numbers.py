60c99011a64a1c609320bc5586ba54131f7e4ac2
Alexander Shelton <alexander.nathan.shelton@outlook.com>
2024-02-06 23:00:12 -0500

    Day 10

---
+++ Day 10/convert_numbers.py
@@ -0,0 +1,9 @@
+# Your new company has a list of figures saved in a database.
+# The issue is that these numbers have no separator.
+# The numbers are saved in a list in the following format:
+# [1000000, 2356989, 2354672, 9878098].  
+# You have been asked to write a code that will convert each of the numbers in the list into a string. 
+# Your code should then add a comma to each number as a thousand separator for readability. 
+# When you run your code on the above list, your output should be:
+# ['1,000,000', '2,356,989', '2,354,672', '9,878,098’]  
+# Write a function called 'convert_numbers' that will take one argument, the list of numbers above.
---
+++ Day 10/hide_password.py
@@ -0,0 +1,3 @@
+# Write a function called 'hide_password' that takes no parameters.
+# The function takes an input (a password) from a user and returns a hidden password.
+# For example, if the user enters "Hello" as a password, the function should return "*****" as a password and tell the user that their password is four characters long.
