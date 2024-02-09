7ed475fdba1f58146333408237942130568aadd0
Alexander Shelton <alexander.nathan.shelton@outlook.com>
2024-02-09 13:43:31 -0500

    Day 23

---
+++ Day 23/multiply_words.py
@@ -0,0 +1,10 @@
+s = 'love live and laugh'
+
+# Write a function called multiply words that takes a string as an argument and multiplies the length of each word in the string by the length of other words in the string.
+# For example, the string above should return '240: love (4), live (4), and laugh (5)'.
+# However, your function should only multiply words with all lowercase letters.
+# If a word has one uppercase letter, it should be ignored.
+# For example, the following string:
+t = 'Hate war love Peace' 
+# Should return '12 - war (3) and love (4)'.
+# Hate and Peace will be ignored because they have at least one uppercase letter.
---
+++ Day 23/simple_calculator.py
@@ -0,0 +1,4 @@
+# Create a simple calculator.
+# The calculator should be able to perform the following basic math operations: add, subtract, divide, and multiply.
+# The calculator should take input from users. 
+# The calculator should be able to handle 'ZeroDivisionError', 'NameError', and 'ValueError'.
