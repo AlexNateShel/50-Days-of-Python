e89b0383802de1932aa43659b2aed17cd9548f79
Alexander Shelton <alexander.nathan.shelton@outlook.com>
2024-02-14 15:15:58 -0500

    Day 35

--- Day 35/find_index.py
+++ Day 35/find_index.py
@@ -4,5 +4,19 @@
 # For example, if the list is [1, 2, 4, 6, 7, 7] and the integer is 7, your code should return [4, 5] as the indexes of 7 in the list.
 # If the list is [1, 2, 4, 6, 7, 7] and the integer is 8, your code should return [8, 8, 8, 8, 8, 8] because 8 is not in the list.
 
-def find_index(list1: list, num: int):
-    pass
+def find_index(arg: list, n: int) -> list:
+    list1 = []
+    for i, val in enumerate(arg):
+        if val == n:
+            list1.append(i)
+        if n not in arg:
+            for j in arg:
+                list1.append(n)
+            return list1
+    
+    return list1
+        
+        
+x = [1, 2, 4, 6, 7, 7]
+print(find_index(x, 7))
+print(find_index(x, 8))
